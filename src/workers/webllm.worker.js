28311919806b72c84ac4b03ed1d969954090635a
Deep Shahane <61946392+Deep-De-coder@users.noreply.github.com>
2026-01-24 15:14:05 -0500

    Fix ESLint error in worker file

--- src/workers/webllm.worker.js
+++ src/workers/webllm.worker.js
@@ -3,6 +3,7 @@ import { WebWorkerMLCEngineHandler } from "@mlc-ai/web-llm";
 
 const handler = new WebWorkerMLCEngineHandler();
 
+// eslint-disable-next-line no-restricted-globals
 self.onmessage = (msg) => {
   handler.onmessage(msg);
 };
